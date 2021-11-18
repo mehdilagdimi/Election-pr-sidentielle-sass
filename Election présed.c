e84ecc07c9df81d9010f2a82169e7b7cdf8390f8
mehdilagdimi <lagdimi.mehdi@gmail.com>
2021-11-18 22:57:27 +0100

    version 0.1 : introduce candidates, electors, and take 1 single vote for every elector in 1st tour

--- Election présed.c
+++ Election présed.c
@@ -3,31 +3,86 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-void displayArr(char arr[][1], int size, int votes ){
-    printf("\nList of candidates :\n");
+#include <string.h>
+#include <stdbool.h>
+
+void displayArr(char arr[][25], int size, int votes[], int pd){
+    if(pd == 0) printf("\nList of candidates :\nID \t|\tCandidate");
+    else if (pd == 1) printf("\nList of electors :\nID \t|\tCIN");
     for(int i = 0; i < size; i++){
-                printf("%s \n", arr[i]);
-        printf("\n");
+        printf("\n%d \t|\t%s", i,arr[i]);
     }
 }
-void populateDatabase(char arr[][1], int size, int pp){
+int searchIndexCIN(char *str, char arr[][10], int size){
+    for(int i = 0; i < size; i++){
+        if(!strcmp(str, arr[i])) return i;
+    }
+    return -1;
+}
+void populateVotes(int arrVotes[], char arrElectors[][10], char arrCandidates[][25], int sizeE, int sizeP){
+    getch();
+    system("cls");
+    bool oneTimeVote[sizeE];
+    int index, i =0;
+    for(i ; i < sizeE; i++){oneTimeVote[i] = false;}
+    i = 0;
+    char str[10];
+    for(i; i < sizeE ; i++){
+       printf("\nEnter your CIN : ");
+       scanf(" %s", &str);
+       index = searchIndexCIN(str, arrElectors, sizeE);
+       if(index == -1) {
+            printf("\nCIN not found. Try again");
+            i--; //in case someone enters an invalid CIN the loop repeats the process of input so we need to decrement the i
+            continue;
+            }
+        if(oneTimeVote[index]) {
+            printf("\nYou already voted. You can't vote again in first tour.");
+            i--;
+            continue;
+        }
+        displayArr(arrCandidates, sizeP, NULL, 1);
+        printf("\nVote for the candidate by ID : ");
+        scanf(" %d", &arrVotes[index]);
+        oneTimeVote[index] = true;
+        //getch();
+        system("cls");
+    }
+}
+void populateDatabase(char arr[][25], int size, int pd){
     int i=0;
-    if(pp == 0){printf("\nProvide the name of candidates  : ");}
-    else if (pp == 1){printf("\nProvide electors CIN  : ")}
+    if(pd == 0){printf("\nProvide the name of candidates  : ");}
+    else if (pd == 1){printf("\nProvide electors CIN  : ");}
     do {
         scanf(" %s", &arr[i]);
         i++;
     }
     while(i < size);
-    displayArr(arr, size, 0);
+    displayArr(arr, size, NULL, pd);
 }
 void main(){
-    int size;
-    printf("This is the election of 2020 for president status.\nEnter the number of candidates : (minimum 5) ");
-    scanf("%d", &size);
-    char arrCandidates[size][1];
+    int sizeCand;
+    int sizeElec;
+
+    do{
+        printf("This is the election of 2020 for president status.\nEnter the number of candidates : (minimum 5) ");
+        scanf("%d", &sizeCand);
+        if(sizeCand < 5) printf("\nThe election can't be held for less than 5 president candidates.");
+    } while(sizeCand < 5);
+
+    do {
+        printf("\nEnter the number of electors: "); //make it dynamic after having a working version
+        scanf("%d", &sizeElec);
+        if(sizeElec < 10) printf("\nThe election can't be held having only 10 electors.");
+    }while (sizeElec < 10);
+    //declare candidates array and electros array
+    char arrCandidates[sizeCand][25];
+    char arrElectors[sizeElec][10];
+    int votesArr[sizeElec];
     //char** elecArr[3];
-    populateDatabase(arrCandidates, size);
+    populateDatabase(arrCandidates, sizeCand, 0);
+    populateDatabase(arrElectors, sizeElec, 1);
+    populateVotes(votesArr, arrElectors, arrCandidates, sizeElec, sizeCand);
 
     /*printf("\nAdresse of arr %d", &arr);
     elecArr[0] = &arr[0];
